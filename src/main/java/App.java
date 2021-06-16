d4bd267055d6e17f7974a9ceff86ce95f0f03be8
Demidao <demid.olenev@gmail.com>
2021-06-17 01:35:25 +0300

    Коммит №1 для задачи 2.1.2 POJO-класс, scope prototype...

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,17 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld beanStreak =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+        Cat catBean =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getName());
+        Cat catBeanStreak =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(catBeanStreak.getName());
+
+        System.out.println(bean == beanStreak);
+        System.out.println(catBean == catBeanStreak);
     }
 }
